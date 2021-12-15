b8ce3af015912df3d21eb281606a8b1ef9b17652
Nakul Rao <nakul_rao@nakuls-mbp.westpoint.edu>
2021-12-15 10:29:44 -0500

    Refactored Code

--- basic.py
+++ basic.py
@@ -3,48 +3,47 @@
 # Nakul Rao
 import math
 
-def basic(a, b, operation):
-    # print("a = ", a)
-    # print("b = ", b)
+def play(a, b, operation):
     if operation == "add":
         c = b + b
     elif operation == "mult":
-        c = a * b
+        c = b * b
     else:
-        print("Unreachable!")
-    # print("c = ", c)
+        print("Invalid Operation")
+        exit()
 
     max_num = max(a,max(b,c))
-    starting_index = max_num
+    starting_i = max_num
 
     final_array = [0] * 400
+    length = len(final_array)
 
-    for index in range(starting_index, len(final_array)):
-        prod = final_array[index - a] * final_array[index - b] * final_array[index - c]
-        final_array[index] = (prod + 1) % 2
+    for n in range(starting_i, length):
+        prod = final_array[n - a] * final_array[n - b] * final_array[n - c]
+        final_array[n] = (prod + 1) % 2
 
     return final_array
 
 def write_to_file(file_name, a, b):
     f = open(file_name, "w")
-    for elem in a:
-        for elemen in b:
+    for element_a in a:
+        for element_b in b:
             f.writelines("a = ")
-            f.writelines(str(elem))
+            f.writelines(str(element_a))
             f.writelines("\n")
             f.writelines("b = ")
-            f.writelines(str(elemen))
+            f.writelines(str(element_b))
             f.writelines("\n")
             f.writelines("c = ")
-            f.writelines(str(elemen + elemen))
+            f.writelines(str(element_b + element_b))
             f.writelines("\n")
             f.writelines("\n")
-            f.writelines(str(basic(elem, elemen, "add")))
+            f.writelines(str(play(element_a, element_b, "add")))
             f.writelines("\n\n")
     print("Written.")
 
 if __name__ == "__main__":
-    a = [10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
+    a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
     b = [1,2,3,4,5]
     write_to_file("output.txt", a, b)
 
